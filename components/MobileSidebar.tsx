7fb08880d9493490eb851773daae92d3da46733c
penguinboots <sabrina.ynw@gmail.com>
2023-07-19 12:32:25 -0700

    build mobile sidebar

---
+++ components/MobileSidebar.tsx
@@ -0,0 +1,20 @@
+"use client";
+import Sidebar from "./Sidebar";
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Menu } from "lucide-react";
+
+export const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
