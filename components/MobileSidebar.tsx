c94a5476bf194e6a127d2d22e4de5e9523786dab
penguinboots <sabrina.ynw@gmail.com>
2023-07-19 12:33:33 -0700

    fix: hydration error

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,10 +1,21 @@
 "use client";
+import { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
 import { Button } from "./ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { Menu } from "lucide-react";
 
 export const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, [])
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <Sheet>
       <SheetTrigger>
