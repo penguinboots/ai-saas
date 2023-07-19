46b1224bccb7309e8a4ff9afa53939f17d3831fe
penguinboots <sabrina.ynw@gmail.com>
2023-07-19 12:05:42 -0700

    build landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
 
 export default function LandingPage() {
   return (
-    <div>LandingPage (Unprotected)</div>
-  )
+    <div>
+      LandingPage (Unprotected)
+      <div>
+        <Link href="/sign-in">
+          <Button>Login</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
