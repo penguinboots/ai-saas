8931ea2a4e6a82b8a04da3c7195bbf20e2843a53
penguinboots <sabrina.ynw@gmail.com>
2023-07-20 10:47:35 -0700

    use conversation structure for image generator

---
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { formSchema } from "./constants";
+import axios from "axios";
+import { Heading } from "@/components/Heading";
+import { ImageIcon } from "lucide-react";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Empty from "@/components/Empty";
+import Loader from "@/components/Loader";
+import { cn } from "@/lib/utils";
+import BotAvatar from "@/components/BotAvatar";
+import UserAvatar from "@/components/UserAvatar";
+
+const ImagePage = () => {
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const response = await axios.post("/api/conversation", {});
+
+      form.reset();
+    } catch (error: any) {
+      // TO DO : open pro modal
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Image Generation"
+        description="Turn a prompt into an image!"
+        icon={ImageIcon}
+        iconColor="text-pink-700"
+        bgColor="bg-pink-700/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="
+                rounded-lg border w-full p-4 px-3 md:px-6
+                focus-within:shadow-sm grid grid-cols-12 gap-2"
+              autoComplete="off"
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="How do I calculate the radius of a circle?"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                disabled={isLoading}
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+              <Loader />
+            </div>
+          )}
+          {messages.length === 0 && !isLoading && (
+            <Empty label="No conversation started." />
+          )}
+          <div>
+            Images rendered here.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImagePage;
