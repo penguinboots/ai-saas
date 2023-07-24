a7a0eb2b93931436e18fe0c7acd4b32b9e397a16
penguinboots <sabrina.ynw@gmail.com>
2023-07-24 16:08:34 -0700

    build pro popup

--- components/ProModal.tsx
+++ components/ProModal.tsx
@@ -1,8 +1,25 @@
+"use client";
+import { useProModal } from "@/hooks/useProModal";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
+import { Badge } from "./ui/badge";
 
 const ProModal = () => {
-  return (
-    <div>ProModal</div>
-  )
-}
+  const proModal = useProModal();
 
-export default ProModal
+  return (
+    <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
+            <div className="flex items-center gap-x-2 font-bold py-1">
+              Upgrade to Genius
+              <Badge className="uppercase text-sm py-1" variant="premium">pro</Badge>
+            </div>
+          </DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ProModal;
