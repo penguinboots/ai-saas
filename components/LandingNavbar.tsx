6bcaca4ba7a3e647b412e4d3e9b2128f7b4ea908
penguinboots <sabrina.ynw@gmail.com>
2023-07-25 13:51:31 -0700

    add login button

--- components/LandingNavbar.tsx
+++ components/LandingNavbar.tsx
@@ -23,7 +23,13 @@ const LandingNavbar = () => {
           Genius
         </h1>
       </Link>
-      <div></div>
+      <div className="flex items-center gap-x-2">
+        <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
+          <Button variant="outline" className="rounded-full">
+            Get Started
+          </Button>
+        </Link>
+      </div>
     </nav>
   );
 };
