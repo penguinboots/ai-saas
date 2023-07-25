95b9fdbfe57fb49ad9480c7c33fe624a5630426f
penguinboots <sabrina.ynw@gmail.com>
2023-07-25 12:59:07 -0700

    add toast errors

--- components/ProModal.tsx
+++ components/ProModal.tsx
@@ -23,6 +23,7 @@ import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 import axios from "axios";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
 
 const tools = [
   {
@@ -67,7 +68,7 @@ const ProModal = () => {
       const response = await axios.get("/api/stripe");
       window.location.href = response.data.url;
     } catch (error) {
-      console.log(error, "STRIPE_CLIENT_ERROR");
+      toast.error("Something went wrong!")
     } finally {
       setLoading(false);
     }
