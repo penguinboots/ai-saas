d3c1e1a15613da18de9b85b86a9771e6d7d9c8ba
penguinboots <sabrina.ynw@gmail.com>
2023-07-24 17:22:31 -0700

    add subscribe function to button

--- components/ProModal.tsx
+++ components/ProModal.tsx
@@ -9,10 +9,20 @@ import {
   DialogTitle,
 } from "./ui/dialog";
 import { Badge } from "./ui/badge";
-import { Check, Code, ImageIcon, MessageSquare, Music, VideoIcon, Zap } from "lucide-react";
+import {
+  Check,
+  Code,
+  ImageIcon,
+  MessageSquare,
+  Music,
+  VideoIcon,
+  Zap,
+} from "lucide-react";
 import { Card } from "./ui/card";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
+import axios from "axios";
+import { useState } from "react";
 
 const tools = [
   {
@@ -49,6 +59,19 @@ const tools = [
 
 const ProModal = () => {
   const proModal = useProModal();
+  const [loading, setLoading] = useState(false);
+
+  const onSubscribe = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("/api/stripe");
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log(error, "STRIPE_CLIENT_ERROR");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -64,24 +87,25 @@ const ProModal = () => {
           </DialogTitle>
           <DialogDescription className="text-center pt-2 space-y-2 text-zinc-900 font-medium">
             {tools.map((tool) => (
-              <Card key={tool.label} className="p-3 border-black/5 flex items-center justify-between">
+              <Card
+                key={tool.label}
+                className="p-3 border-black/5 flex items-center justify-between"
+              >
                 <div className="flex items-center gap-x-4">
                   <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
-                    <tool.icon className={cn("w-6 h-6", tool.color)}/>
+                    <tool.icon className={cn("w-6 h-6", tool.color)} />
                   </div>
-                  <div className="font-semibold text-sm">
-                    {tool.label}
-                  </div>
+                  <div className="font-semibold text-sm">{tool.label}</div>
                 </div>
-                <Check className="text-primary w-5 h-5"/>
+                <Check className="text-primary w-5 h-5" />
               </Card>
             ))}
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button size="lg" variant="premium" className="w-full">
+          <Button onClick={onSubscribe} size="lg" variant="premium" className="w-full">
             Upgrade
-            <Zap className="w-4 h-4 ml-2 fill-white"/>
+            <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>
         </DialogFooter>
       </DialogContent>
